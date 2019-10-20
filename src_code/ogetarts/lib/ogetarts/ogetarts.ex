57f206bd4d972ef2528175f42e82625a4dc50a40
jakemittlemanwebdev2019 <megamanzerox@gmail.com>
2019-10-20 13:46:04 -0400

    attacking works a little. There's a couple of cases to take care of

--- src_code/ogetarts/lib/ogetarts/ogetarts.ex
+++ src_code/ogetarts/lib/ogetarts/ogetarts.ex
@@ -26,107 +26,241 @@ defmodule Ogetarts.Game do
 
     def change_board_row(game, board, row, new_data, i, j) do
 
-    new_row = Enum.map_reduce(row, 0, fn piece, index ->
-        if (index == (j)) do
-            if (length(new_data) != 0) do
-                # If we're not removing a piece
-                # we have to update its i and j
-                updated_data = List.replace_at(new_data, 3, i)
-                updated_data = List.replace_at(updated_data, 4, j)
-                {updated_data, index + 1}
-            else
-                {new_data, index + 1}
-            end
-        else
-            {piece, index + 1}
-        end
+      new_row = Enum.map_reduce(row, 0, fn piece, index ->
+          if (index == (j)) do
+              if (length(new_data) != 0) do
+                  # If we're not removing a piece
+                  # we have to update its i and j
+                  updated_data = List.replace_at(new_data, 3, i)
+                  updated_data = List.replace_at(updated_data, 4, j)
+                  {updated_data, index + 1}
+              else
+                  {new_data, index + 1}
+              end
+          else
+              {piece, index + 1}
+          end
     end)
 
-    # We're dumb and didn't even look at replace_at.
-    # Now we don't have to insert/delete. We just replace.
-    # Can't believe we missed that for like 72 hours
     List.replace_at(board, i, elem(new_row,0))
+  end
+
+  # If the Attacking Piece wins, we put attacker in attacked slot
+  # and make the old attacker's slot []
+  # update_counts is a boolean and if it's true we update counts
+  # if it's false, we don't. Player is the player to update counts
+  # for
+  def resolve_attacker_wins(game, attacking_piece, attacked_piece,
+                    update_counts, player) do
+
+    old_i = Enum.at(attacking_piece, 3)
+    old_j = Enum.at(attacking_piece, 4)
+    new_i = Enum.at(attacked_piece, 3)
+    new_j = Enum.at(attacked_piece, 4)
+
+    board = game.board
+    insert_row = Enum.at(board, new_i)
+
+    board = change_board_row(game, board, insert_row,
+                            game.last_click, new_i, new_j)
+
+    delete_row = Enum.at(board, old_i)
+    board = change_board_row(game, board, delete_row,
+                            [], old_i, old_j)
+
+    if (update_counts) do
+      if (player == 1) do
+        Map.merge(game, %{last_click: [], board: board,
+                          p1_piece_count: (game.p1_piece_count - 1)})
+      else
+        Map.merge(game, %{last_click: [], board: board,
+                          p2_piece_count: (game.p2_piece_count - 1)})
+      end
+    else
+      Map.merge(game, %{last_click: [], board: board})
+    end
+  end
+
+  # If the defending piece wins, we remove the attacker form the board
+  # since flags and bombs can't attack we always decrement piece count
+  def resolve_attacked_wins(game, attacking_piece, player) do
+
+    i = Enum.at(attacking_piece, 3)
+    j = Enum.at(attacking_piece, 4)
+
+    board = change_board_row(game, game.board, Enum.at(game.board, i), [], i, j)
+
+    if (player == 1) do
+      Map.merge(game, %{last_click: [], board: board,
+                        p1_piece_count: (game.p1_piece_count - 1)})
+    else
+      Map.merge(game, %{last_click: [], board: board,
+                        p2_piece_count: (game.p2_piece_count - 1)})
+    end
+
+  end
+
+
+  def attack(game, attacked_piece) do
+      #TODO
+      # bombs(rank 11) kill everyone except miners(rank 3)
+      # attacking flag(rank 12) should set flag_found state variable and end the game
+      # marshall(rank 10) can only be killed by spy(rank 1)
+      # Decrement p1 or p2 piece count, if necessary
+      # pieces of equal rank attacking each other both are killed
+
+      attacking_piece = game.last_click
+      attacking_piece_rank = Enum.at(attacking_piece,1)
+      attacking_piece_player = Enum.at(attacking_piece, 2)
+
+      attacked_piece_rank = Enum.at(attacked_piece, 1)
+      attacked_piece_player = Enum.at(attacked_piece, 2)
+
+
+      # the piece swapping should probably be its own function
+
+      cond do
+          # flag is attacked
+          attacked_piece_rank == 12 ->
+              #TODO
+              # set flag_found, send alert, end game
+              true
+
+          # bomb is attacked by miner
+          (attacked_piece_rank == 11) && (attacking_piece_rank == 3) ->
+              #TODO
+              # remove attacked piece from the board
+              # DO NOT decrement piece counts
+              # a) Change Miner's i and j values
+              # old_i = Enum.at(attacking_piece, 3)
+              # old_j = Enum.at(attacking_piece, 4)
+              # new_i = Enum.at(attacked_piece, 3)
+              # new_j = Enum.at(attacked_piece, 4)
+              #
+              # board = game.board
+              # insert_row = Enum.at(board, new_i)
+              #
+              # board = change_board_row(game, board, insert_row, game.last_click, new_i, new_j)
+              #
+              # delete_row = Enum.at(board, old_i)
+              # board = change_board_row(game, board, delete_row,
+              #                         [], old_i, old_j)
+              #
+              # Map.merge(game, %{last_click: [], board: board})
+              #def resolve_attack(game, attacking_piece, attacked_piece,
+              #                  update_counts, player) do
+              resolve_attacker_wins(game, attacking_piece,
+                                    attacked_piece, false, 0)
+
+
+          # bomb is attacked by non-miner
+          (attacked_piece_rank == 11) && (attacking_piece_rank != 3) ->
+              #TODO
+              # remove attacking piece from the board
+              # decrement attacking_piece_rank piece count
+              if (attacking_piece_player == 1) do
+                resolve_attacked_wins(game, attacking_piece, 1)
+              else
+                resolve_attacked_wins(game, attacking_piece, 2)
+              end
+
+          # marhsall attacked by spy
+          (attacked_piece_rank == 10) && (attacking_piece_rank == 1) ->
+              #TODO
+              # remove attacked_piece from the board
+              # decrement attacked_piece_rank's piece count
+              if (attacked_piece_player == 1) do
+                resolve_attacker_wins(game, attacking_piece, attacked_piece, true, 1)
+              else
+                resolve_attacker_wins(game, attacking_piece, attacked_piece, true, 2)
+              end
+
+          # pieces of equal rank kill each other
+          (attacked_piece_rank == attacking_piece_rank) ->
+              #TODO
+              # remove both pieces from the board
+              # decrement both p1 and p2 piece count
+              true
+
+          # pieces of different ranks
+          (attacked_piece_rank != attacking_piece_rank) ->
+              #TODO
+              if (attacking_piece_rank > attacked_piece_rank) do
+                  # remove attacked_piece
+                  # decrement attacked_piece_rank's piece count
+                  if (attacked_piece_player == 1) do
+                    resolve_attacker_wins(game, attacking_piece, attacked_piece, true, 1)
+                  else
+                    resolve_attacker_wins(game, attacking_piece, attacked_piece, true, 2)
+                  end
+              else
+                  # remove attacking_piece
+                  # decrement attacking_piece_rank's piece count
+                  if (attacking_piece_player == 1) do
+                    resolve_attacked_wins(game, attacking_piece, 1)
+                  else
+                    resolve_attacked_wins(game, attacking_piece, 2)
+                  end
+              end
+
+          # default
+          true ->
+              true
+      end
+  end
+
+  # this function assumes that the i,j positions passed to it
+  # are legal (only horizontal or vertical)
+  def checkScoutLegalMove(board, old_i, old_j, new_i, new_j) do
+    # since we're going to check each position from where
+    # the scout moves to where he finishes, we need to check
+    # that the way is cleared and easier to do this with
+    # a range from lower num -> higher num
+
+    if (old_i == new_i) do
+      from_position = min(old_j, new_j)
+      to_position = max(old_j, new_j)
+
+      # this gets the row that the piece is moving in
+      # then it slices it + and - 1 (we just need to check
+      # if there are pieces in the intermediary positions)
+      row = Enum.at(board, old_i)
+      movement_range = Enum.slice(row, from_position+1..to_position-1)
+
+      # this immediately returns true if the function
+      # returns true. But we want the function to return
+      # false if it CAN'T move, so that's why we reverse
+      # the result
+      canMove = Enum.any?(movement_range, fn piece ->
+          piece != []
+        end)
+      !canMove
+
+
+    else
+      from_position = min(old_i, new_i)
+      to_position = max(old_i, new_i)
+
+      column = Enum.map((from_position+1)..(to_position-1), fn index ->
+        # (Get the piece (from the row it's in) at j)
+        # -> [[],[],[],[],[],[],[],[],[],[]] (get row)
+        #         |
+        #         v
+        #    [[],[],[],[],[],[],[],[],[],[]] (then get the piece)
+        Enum.at(Enum.at(board, index), old_j)
+      end)
+
+      canMove = Enum.any?(column, fn piece ->
+          piece != []
+        end)
+
+      !canMove
+    end
   end
 
 
 
-    def attack(game, attacked_piece) do
-        #TODO
-        # bombs(rank 11) kill everyone except miners(rank 3)
-        # attacking flag(rank 12) should set flag_found state variable and end the game
-        # marshall(rank 10) can only be killed by spy(rank 1)
-        # Decrement p1 or p2 piece count, if necessary
-        # pieces of equal rank attacking each other both are killed
-
-        attacking_piece = game.last_click
-        attacking_piece_rank = Enum.at(attacking_piece,1)
-        attacking_piece_player = Enum.at(attacking_piece, 2)
-
-        attacked_piece_rank = Enum.at(attacked_piece, 1)
-        attacked_piece_player = Enum.at(attacked_piece, 2)
-
-        cond do
-            # flag is attacked
-            attacked_piece_rank == 12 ->
-                #TODO
-                # set flag_found, send alert, end game
-                true
-
-            # bomb is attacked by miner
-            (attacked_piece_rank == 11) && (attacking_piece_rank == 3) ->
-                #TODO
-                # remove attacked piece from the board
-                # DO NOT decrement piece counts
-                true
-
-            # bomb is attacked by non-miner
-            (attacked_piece_rank == 11) && (attacking_piece_rank != 3) ->
-                #TODO
-                # remove attacking piece from the board
-                # decrement attacking_piece_rank piece count
-                true
-
-            # marhsall attacked by spy
-            (attacked_piece_rank == 10) && (attacking_piece_rank == 1) ->
-                #TODO
-                # remove attacked_piece from the board
-                # decrement attacked_piece_rank's piece count
-                true
-
-            # pieces of equal rank kill each other
-            (attacked_piece_rank == attacking_piece_rank) ->
-                #TODO
-                # remove both pieces from the board
-                # decrement both p1 and p2 piece count
-                true
-
-            # pieces of different ranks
-            (attacked_piece_rank != attacking_piece_rank) ->
-                #TODO
-                if (attacking_piece_rank > attacked_piece_rank) do
-                    # remove attacked_piece
-                    # decrement attacked_piece_rank's piece count
-                    true
-                else
-                    # remove attacking_piece
-                    # decrement attacking_piece_rank's piece count
-                    true
-                end
-
-            # default
-            true ->
-                true
-        end
-
-
-
-
-    end
-
-
-
-  def is_legal_move(game, piece, i, j) do
-    #TODO: when moving a 2, we need to check if there are pieces in between. 2 should not be able to jump over pieces
+  def is_legal_move(game, to_spot, i, j) do
 
     last_i = Enum.at(game.last_click, 3)
     last_j = Enum.at(game.last_click, 4)
@@ -134,12 +268,18 @@ defmodule Ogetarts.Game do
     board = game.board
     # Get the player number, 1 or 2, of the last clicked piece, and next clicked piece
     last_player_piece = Enum.at(game.last_click,2)
-    current_player_piece = Enum.at(piece,2)
+    current_player_piece = Enum.at(to_spot,2)
+
+    IO.puts("Here")
 
     cond do
         # scout (rank 2) movement is horizontal or vertical, unlimited spaces
         (last_rank == 2) && ((last_i == i) || (last_j == j)) && last_player_piece != current_player_piece ->
-            true
+            (if (abs(last_i - i) == 1 || abs(last_j - j) == 1) do
+              true
+            else
+              checkScoutLegalMove(board, last_i, last_j, i, j)
+            end)
 
         # any piece can move horizontally or vertially once space. Bombs and flags
         # are restricted in move_piece to prevent last_click being set if a player
@@ -161,7 +301,7 @@ defmodule Ogetarts.Game do
         # This logic isn't quite right, but it's
         # on the right track i think? Currently it's deleting the piece, instead
         # of just clearing out last_click
-        game.last_click == piece ->
+        game.last_click == to_spot ->
             Map.merge(game, %{last_click: [], board: board})
 
         # Cond statements need at least one statement to be truthy. We can't
@@ -193,32 +333,35 @@ defmodule Ogetarts.Game do
             #TODO:
             # We should probably trigger the attack logic here?
 
+            if (length(piece) != 0) do
+              attack(game, piece)
+            else
+              board = game.board
+              # This is the i value in last_click so we don't have to
+              # keep calling Enum.at.
+              # The (i, j) pair passed into the function is for the
+              # insert row.
+              delete_i = Enum.at(game.last_click, 3)
+              # this is the same for j
+              delete_j = Enum.at(game.last_click, 4)
 
-            board = game.board
-            # This is the i value in last_click so we don't have to
-            # keep calling Enum.at.
-            # The (i, j) pair passed into the function is for the
-            # insert row.
-            delete_i = Enum.at(game.last_click, 3)
-            # this is the same for j
-            delete_j = Enum.at(game.last_click, 4)
+              insert_row = Enum.at(board, i)
 
-            insert_row = Enum.at(board, i)
+              # This is the duplicated code, just in a function now
+              # With all the params honestly not sure it's better, but
+              # code at least isn't duplicated now....
+              board = change_board_row(game, board, insert_row, game.last_click, i, j)
 
-            # This is the duplicated code, just in a function now
-            # With all the params honestly not sure it's better, but
-            # code at least isn't duplicated now....
-            board = change_board_row(game, board, insert_row, game.last_click, i, j)
+              # very important that this is after we insert.
+              # Otherwise it's taking an old state if you're moving a piece
+              # in the same row.
+              delete_row = Enum.at(board, delete_i)
 
-            # very important that this is after we insert.
-            # Otherwise it's taking an old state if you're moving a piece
-            # in the same row.
-            delete_row = Enum.at(board, delete_i)
+              board = change_board_row(game, board, delete_row,
+                                      [], delete_i, delete_j)
 
-            board = change_board_row(game, board, delete_row,
-                                    [], delete_i, delete_j)
-
-            Map.merge(game, %{last_click: [], board: board})
+              Map.merge(game, %{last_click: [], board: board})
+            end
         else
             game
         end
